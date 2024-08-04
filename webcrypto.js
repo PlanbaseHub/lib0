bb471f6007e2962106662aa03cb6f335eb367f84
Scott Huang <scott890719@gmail.com>
2024-08-03 19:57:07 -0700

    modify webcrypto

--- webcrypto.js
+++ webcrypto.js
@@ -1,4 +1,6 @@
 /* eslint-env browser */
+import { polyfillWebCrypto } from "expo-standard-web-crypto";
+polyfillWebCrypto();
 
 export const subtle = crypto.subtle
 export const getRandomValues = crypto.getRandomValues.bind(crypto)
